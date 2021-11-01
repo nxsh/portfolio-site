975c30acbe85b620db116ca2bdaf2118a2f75008
nxsh <ryan@renash.co.uk>
2021-11-01 22:18:42 +0000

    PDF viewer responsive but requires CloudPDF

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import CloudPdfViewer from "@openbook/cloudpdf-viewer";
+
 import Layout from "../components/layout";
 import * as cvStyles from "./cv.module.scss";
 import Head from "../components/head";
@@ -6,12 +8,21 @@ import Head from "../components/head";
 import cvPDF from "../assets/cv.pdf";
 
 const CVPage = () => {
+  const viewer = useRef(null);
+  useEffect(() => {
+    CloudPdfViewer(
+      {
+        documentId: "61f0be8d-c0cf-44db-80ec-82f55757a662",
+        darkMode: true,
+      },
+      viewer.current
+    ).then((instance) => {});
+  }, []);
   return (
     <Layout>
       <Head title="CV" />
-
       <div className={cvStyles.container}>
-        <iframe src={`${cvPDF}#view=fitH`} title="RN_CV" />
+        <div className={cvStyles.viewer} ref={viewer}></div>
       </div>
     </Layout>
   );
